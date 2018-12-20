ac44b1cb866a1bb3730ddc6199b3d1643888fb77
deepkanwal <deepkanwal@gmail.com>
2018-12-20 00:33:06 -0500

    add vimrc and clean up

--- install.sh
+++ install.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-############################
+
 # .install.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
+# This script creates symlinks from the home directory to any desired dotfiles in ~/.dotfiles
 
-########## Variables
+# variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="zshrc oh-my-zsh gitconfig"    # list of files/folders to symlink in homedir
-
-##########
+dir=~/.dotfiles                    
+olddir=~/.dotfiles_old             	
+files="zshrc vimrc"    
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
@@ -24,8 +21,8 @@ echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    echo "Moving existing $file to $olddir"
+    mv ~/.$file $olddir
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
---
+++ vimrc
@@ -0,0 +1,12 @@
+" Tabs
+set tabstop=2
+set shiftwidth=2
+set softtabstop=2
+set expandtab
+
+" Plugins
+call plug#begin('~/.vim/plugged')
+
+Plug 'sheerun/vim-polyglot'
+
+call plug#end()
